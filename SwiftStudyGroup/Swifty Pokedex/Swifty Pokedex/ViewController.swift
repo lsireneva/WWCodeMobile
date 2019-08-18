4216a574b9625f2ac9b86ec15cfba92c03a00155
Vivian Smiari <vsmiari@gmail.com>
2019-08-18 13:08:36 +0300

    Initial commit for the Swifty Pokedex application This application will utilise the public RESTful Pokémon API, PokéAPI. The purpose of this application is to learn and practice on the Swift language.

---
+++ SwiftStudyGroup/Swifty Pokedex/Swifty Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty Pokedex
+//
+//  Created by Paraskevi Smiari on 18/08/2019.
+//  Copyright © 2019 Paraskevi Smiari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
