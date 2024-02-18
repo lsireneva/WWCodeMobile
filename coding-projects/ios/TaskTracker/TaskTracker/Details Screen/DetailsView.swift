805b9392843213a7da7f9fe85125abb4b8a3c36e
Itunu Raimi <chavitunu@gmail.com>
2024-02-18 17:14:47 +0100

    add delete confirmation popup view

--- coding-projects/ios/TaskTracker/TaskTracker/Details Screen/DetailsView.swift
+++ coding-projects/ios/TaskTracker/TaskTracker/Details Screen/DetailsView.swift
@@ -153,6 +153,55 @@ struct DetailsScreen: View {
             .cornerRadius(25)
         }
     }
+
+    private struct DeleteConfirmationPopupView: View {
+        @Environment(\.presentationMode) var presentationMode
+        @Binding var showDeleteConfirmationPopup: Bool
+
+        var body: some View {
+            ZStack {
+                VStack {
+                    Text("Are you sure you want to delete this entry?")
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .padding()
+
+                    HStack {
+                        Button("Ok") {
+                            // TODO: Delete Timed Activity #80
+                            self.showDeleteConfirmationPopup = false
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                        .frame(minWidth: 0, maxWidth: 200)
+                        .font(.system(size: 20))
+                        .padding()
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(5)
+
+
+                        .padding()
+
+                        Button("Cancel") {
+                            self.showDeleteConfirmationPopup = false
+                        }
+                        .frame(minWidth: 0, maxWidth: 200)
+                        .font(.system(size: 18))
+                        .padding()
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(5)
+                        .padding()
+                    }
+                }
+                .background(Color.white)
+            }
+            .frame(width: 300, height: 200)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+
+        }
+    }
 }
 
 #Preview {
