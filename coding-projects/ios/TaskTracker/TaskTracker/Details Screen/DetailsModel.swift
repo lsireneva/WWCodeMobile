e0c5ecfd113919ef74e8b184d46c21d6c132b695
Su Thin <suthazinthin@gmail.com>
2024-02-19 15:19:47 -0500

    Add Timed Activity on DetailsModel screen referencing add method  from DetailsViewModel and create an Activity object

--- coding-projects/ios/TaskTracker/TaskTracker/Details Screen/DetailsModel.swift
+++ coding-projects/ios/TaskTracker/TaskTracker/Details Screen/DetailsModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Activity: Identifiable, Codable {
+    var id = UUID()
+    var activityName: String
+    var date: Date
+    var startTime: Date
+    var endTime: Date
+    
+    private func formatTime(_ date: Date) -> String {
+          DateFormatter.localizedString(from: date, dateStyle: .none, timeStyle: .medium)
+      }
+    
+    // Calculate the difference between endTime and startTime and return duration
+    var duration: String {
+            let duration = endTime.timeIntervalSince(startTime)
+            let hours = Int(duration) / 3600
+            let minutes = Int(duration) / 60 % 60
+            let seconds = Int(duration) % 60
+            return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+        }
+}
