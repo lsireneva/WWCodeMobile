770981e7f1494c671499b413212ab9b0c9ae4bd5
ThanaReka <r_thanabalan@yahoo.ca>
2024-03-07 10:04:52 -0800

    optimized imports after implementing changes

--- coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskSettings/TaskSettingsScreen.kt
+++ coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskSettings/TaskSettingsScreen.kt
@@ -3,7 +3,6 @@ package com.example.tasktracker.ui.TaskSettings
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,14 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,12 +25,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tasktracker.R
-import com.example.tasktracker.ui.TaskList.TaskListScreen
 
 
 @OptIn(ExperimentalMaterial3Api::class)
