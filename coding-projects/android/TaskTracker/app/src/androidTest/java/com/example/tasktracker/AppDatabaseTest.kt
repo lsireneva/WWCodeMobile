adeef6fb95c1d076ecd8100e14936cc84269f6dd
jyoshna-joshi <79568910+jyoshna-joshi@users.noreply.github.com>
2024-03-17 10:08:20 +1100

    code refinements

--- coding-projects/android/TaskTracker/app/src/androidTest/java/com/example/tasktracker/AppDatabaseTest.kt
+++ coding-projects/android/TaskTracker/app/src/androidTest/java/com/example/tasktracker/AppDatabaseTest.kt
@@ -6,6 +6,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.tasktracker.data.AppDatabase
 import com.example.tasktracker.data.TaskDAO
+import com.example.tasktracker.data.TaskRepository
 import com.example.tasktracker.data.model.Task
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
@@ -27,7 +28,7 @@ import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
 class AppDatabaseTest {
-
+    private lateinit var repository: TaskRepository
     private lateinit var database: AppDatabase
     private lateinit var taskDAO: TaskDAO
 
@@ -36,9 +37,9 @@ class AppDatabaseTest {
         // Context of the app under test.
         val context = ApplicationProvider.getApplicationContext<Context>()
         database = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
-            .allowMainThreadQueries()
             .build()
         taskDAO = database.taskDAO()
+        repository = TaskRepository(taskDAO)
     }
 
     @After
@@ -56,10 +57,10 @@ class AppDatabaseTest {
         val task = Task(activityName = "Test Activity", date = "2024-03-04", startTimeInMillis = 500, endTimeInMillis = 1000, duration = "1h")
 
         // When inserting a task
-        taskDAO.insertTask(task)
+        repository.insertTask(task)
 
         // Then the task can be retrieved
-        val allTasks: List<Task> = getOrAwaitValueMainThread(taskDAO.getAllTasks())
+        val allTasks: List<Task> = getOrAwaitValueMainThread(repository.allTasks)
         assertEquals("Test Activity", allTasks[0].activityName)
     }
 
@@ -70,7 +71,7 @@ class AppDatabaseTest {
         val mainScope = MainScope()
         mainScope.launch {
             flowData
-                .flowOn(Dispatchers.Main)
+                .flowOn(Dispatchers.IO)
                 .collect{value ->
                     data = value as T
                     latch.countDown()
