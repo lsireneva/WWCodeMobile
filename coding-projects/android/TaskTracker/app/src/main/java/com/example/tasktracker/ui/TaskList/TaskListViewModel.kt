c18f63bf3f0d37e9b4c29b782c02d959283e66a9
ThanaReka <r_thanabalan@yahoo.ca>
2024-03-22 21:14:35 -0700

    update TaskListViewModel to take in flow

--- coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskList/TaskListViewModel.kt
+++ coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskList/TaskListViewModel.kt
@@ -1,60 +1,19 @@
 package com.example.tasktracker.ui.TaskList
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.tasktracker.data.TaskRepository
+import com.example.tasktracker.data.TaskDAO
 import com.example.tasktracker.data.model.Task
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 /**
  * Created by Gauri Gadkari on 1/23/24.
  */
 @HiltViewModel
-class TaskListViewModel @Inject constructor(private var respository: TaskRepository) : ViewModel() {
+class TaskListViewModel @Inject constructor(private val taskDao: TaskDAO) : ViewModel() {
+    fun getAllTasks(): Flow<List<Task>> = taskDao.getAllTasks()
 
-    /**
-     * following hardcoded strings should eventually be replaced by data based on user input once
-     * app persistence is implemented and TaskRepository is modified to retrieve data from the DAO
-     */
-    fun getAllTasks(): Flow<List<Task>> = flowOf(
-        listOf(
-            Task(
-                1,
-                "Walking",
-                "Yesterday",
-                "0",
-                "0",
-                "01:35:08"
-            ),
-            Task(
-                2,
-                "finishing certifications",
-                "Yesterday",
-                "0",
-                "0",
-                "03:40:04"
-            ),
-            Task(
-                3,
-                "setting up new dryer unit",
-                "December 19, Tuesday",
-                "0",
-                "0",
-                "01:20:21"
-            ),
-            Task(
-                4,
-                "coding crunch time",
-                "December 19, Tuesday",
-                "0",
-                "0",
-                "09:30:10"
-            )
-        )
-    )
- }
+}
+
+
