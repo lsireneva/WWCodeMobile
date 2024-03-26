b0d356ba2cad385a688b4be0bf2ff1ac237fdae8
Su Thin <suthazinthin@gmail.com>
2024-03-26 10:19:51 -0400

    Added sort by descending in TaskListViewModel inside getAllTasks function referencing TimeUtil.convertDateToMillis

--- coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskList/TaskListViewModel.kt
+++ coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskList/TaskListViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.tasktracker.ui.TaskList
 
 import androidx.lifecycle.ViewModel
+import com.example.tasktracker.TimeUtil
 import com.example.tasktracker.data.TaskDAO
 import com.example.tasktracker.data.model.Task
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 /**
@@ -13,6 +15,11 @@ import javax.inject.Inject
 @HiltViewModel
 class TaskListViewModel @Inject constructor(private val taskDao: TaskDAO) : ViewModel() {
     fun getAllTasks(): Flow<List<Task>> = taskDao.getAllTasks()
+            .map { tasks ->
+                tasks.sortedByDescending { task ->
+                    TimeUtil.convertDateToMillis(task.date)
+                }
+            }
 
 }
 
